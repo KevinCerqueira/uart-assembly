1804bf1765c655aed0e9b906bf1b5eb1f3d33aa6
KevinCerqueira <kevin.cerqueira@credpago.com>
2022-03-25 14:00:53 -0300

    traduzindo pra pt-br

--- enable_uart.s
+++ enable_uart.s
@@ -5,16 +5,6 @@ _start:
 
 main:
 BL InitializeUART
-BL ReadChar
-
-ReadChar:
-LDR R0, =AUX_MU_LSR_REG
-LDR R1, [R0]
-TST R1, #1
-BEQ ReadChar
-LDR R0, =AUX_MU_IO_REG
-LDR R1, [R0]        
-MOV pc, lr
 
 .equ AUX_ENABLES, 0x7E215004
 .equ AUX_MU_IER_REG, 0x7E215044
@@ -30,80 +20,96 @@ MOV pc, lr
 
 InitializeUART:
 PUSH {lr}
-@ Enable UART
+@ Habilitando UART
 MOV R1, #1
 LDR R0, =AUX_ENABLES
 STR R1, [R0]
-@ disable interrupts
+
+@ Desabilitando interruptores
 MOV R1, #0
 LDR R0, =AUX_MU_IER_REG
 STR R1, [R0]
-@ disable transmit/receive
+
+@ Desabilitando transmissor/receptor
 MOV R1, #0
 LDR R0, =AUX_MU_CNTL_REG
 STR R1, [R0]
-@ set 8 bits communication
+
+@ Setando 8 bits para comunicação
 MOV R1, #3
 LDR R0, =AUX_MU_LCR_REG
 STR R1, [R0]
-@ set the RTS line high
+
+@ Setando RTS linha de nivel alto
 MOV R1, #0
 LDR R0, =AUX_MU_MCR_REG
 STR R1, [R0]
-@ leave disable interrupts
+
+@ Deixando desabilitado os interruptores
 MOV R3, #0
 LDR R0, =AUX_MU_IER_REG
 STR R3, [R0]
-@ clear the input and output buffers
+
+@ Limpando os buffers (entrada/saida)
 MOV R1, #198
 LDR R0, =AUX_MU_IIR_REG
 STR R1, [R0]
-@ set BAUD = 270
+
+@ Setando BAUD = 270
 MOV R1, #200
 ADD R1, R1, #70
 LDR R0, =AUX_MU_BAUD_REG
 STR R1, [R0]
-@ Set GPIO line 14 for transmission (TXD)
+
+@ Setando GPIO linha 14 para transmissao (TXD)
 LDR R0, =GPFSEL1
 LDR R1, [R0]
 BIC R3, R1, #28672
 ORR R3, R3, #8192
 STR R3, [R0]
-@ Set GPIO line 15 for receiving (RXD)
+
+@ Setando GPIO linha 15 para recebimento (RXD)
 LDR R1, [R0]
 BIC R3, R1, #229376
 ORR R3, R3, #65536
 STR R3, [R0]
-@ disable GPIO pull-up/down
+
+@ Desabilitando GPIO pull-up/down
 MOV R1, #0
 LDR R0, =GPPUD
 STR R1, [R0]
-@ wait for 150 cycles
+
+@ Espetando por 150 ciclos
 MOV R1, #0
 cycle1: ADD R1, R1, #1
 BL Cycle
 CMP R1, #150
 BNE cycle1
+
 @ Assert clock lines (14 & 15)
 MOV R1, #16384  @ 1<<14
 LDR R0, =GPPUDCLK0
 STR R1, [R0]
 MOV R1, #32768 @ 1<<15
 STR R1, [R0]
+
 @ wait for 150 cycles
 MOV R1, #0
 cycle2: ADD R1, R1, #1
 BL Cycle
 CMP R1, #150
 BNE cycle2
-@ clear clock lines
+
+@ Limpando linhas do clock
 MOV R1, #0
 LDR R0, =GPCLR0
 STR R1, [R0]
-@ enable bits 0 and 1 in CONTROL
+
+@ Habilitando bits 0 e 1 no CONTROL
 MOV R1, #3
 LDR R0, =AUX_MU_CNTL_REG
 STR R1, [R0]
+
 @ return
 POP {pc}        
 
