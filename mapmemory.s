f1d5cbc1aec3f016e9fb57a53e19294856cbdb97
Helmeppun <52864839+Helmeppun@users.noreply.github.com>
2022-03-30 17:19:10 -0300

    update

--- mapmemory.s
+++ mapmemory.s
@@ -19,8 +19,26 @@ _start:
 	mov r7, #sys_mmap2
 	svc 0
 	mov r8, r0
-	
-	
+
+	mov r2, r8 @ address of gpio regs
+	add r2, #setregoffset @ off to set reg
+	mov r0, #1 @ 1 bit to shift into pos
+	ldr r3, =\pin @ base of pin info table
+	add r3, #8 @ add offset for shift amt
+	ldr r3, [r3] @ load shift from table
+	lsl r0, r3 @ do the shift
+	str r0, [r2] @ write to the register
+
+	mov r2, r8 @ address of gpio regs
+	add r2, #clrregoffset @ off set of clr reg
+	mov r0, #1 @ 1 bit to shift into pos
+	ldr r3, =\pin @ base of pin info table
+	add r3, #8 @ add offset for shift amt
+	ldr r3, [r3] @ load shift from table
+	lsl r0, r3 @ do the shift
+	str r0, [r2] @ write to the register
+
+
 section .data
 timespecsec: .word 0
 timespecnano: .word 100000000
@@ -29,4 +47,15 @@ memOpnErr: .asciz "Failed to open /dev/mem\n"
 memOpnsz: .word .-memOpnErr
 memMapErr: .asciz "Failed to map memory\n"
 memMapsz: .word .-memMapErr
+
 gpioaddr: .word 0x7E20100
+
+pin17: .word 4 @ offset to select register
+.word 21 @ bit offset in select register
+.word 17 @ bit offset in set & clr register
+pin22: .word 8 @ offset to select register
+.word 6 @ bit offset in select register
+.word 22 @ bit offset in set & clr register
+pin27: .word 8 @ offset to select register
+.word 21 @ bit offset in select register
+.word 27 @ bit offset in set & clr registe
