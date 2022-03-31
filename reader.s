2c061dcc34fa4ce0446aa59308292ec3a4233029
Helmeppun <52864839+Helmeppun@users.noreply.github.com>
2022-03-31 10:59:48 -0300

    Update reader.s

--- reader.s
+++ reader.s
@@ -3,25 +3,10 @@
 .equ O_CREAT, 0100
 .equ S_RDWR, 0666
 
-.include "unistd.s"
-
 .macro openFile fileName, flags
 	ldr r0, =\fileName
 	mov r1, #\flags
-	Chapter 7 Linux Operating SyStem ServiCeS
-	136
 	mov r2, #S_RDWR @ RW access rights
 	mov r7, #sys_open
 .endm
 
-
-.macro flushClose fd
-@fsync syscall
-	mov r0, \fd
-	mov r7, #sys_fsync
-	svc 0
-@close syscall
-	mov r0, \fd
-	mov r7, #sys_close
-	svc 0
-.endm
