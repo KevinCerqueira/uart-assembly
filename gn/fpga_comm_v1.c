af002403fd8328d105db0f32e5d7b0b5a7243299
Helmeppun <52864839+Helmeppun@users.noreply.github.com>
2022-05-12 11:12:03 -0300

    Update fpga_comm_v1.c

--- gn/fpga_comm_v1.c
+++ gn/fpga_comm_v1.c
@@ -1,12 +1,38 @@
 #include <stdio.h>
+#include <time.h>
 
-extern void begin_uart();
+void delay(int ms){
+	long pause;
+	clock_t now, then;
+	pause = ms*(CLOCKS_PER_SEC/1000);
+	while( (now-then) < pause);
+		now = clock();
+}
+
+extern void map_mem();
+extern void set_uart();
+extern void put_data(); 
 
 int main(){
+	int delay_time;
 	
+	printf("Your delay time?");
+	/* scanf("%d", delay_time); */
+
+	map_mem();
+	/* printf("Your life is worthless!\n"); */
+
+	set_uart();
+	/* printf("You serve ZERO purpose!\n"); */
+
 	asm("mov r6, #5");
-	for(int i = 0; i < 20000000; i++){
-		begin_uart();	
+	while(1){
+		put_data();
+		printf("You should kill yourself... NOW!\n");
+		delay(1000);
+		
 	}
+	/* printf("You should kill yourself... NOW!\n"); */
+
 	return 0;
 }
